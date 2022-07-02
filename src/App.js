5e5c035f3e337b193a24b645ac298744e582f652
skim73 <55114034+skim73@users.noreply.github.com>
2022-07-02 16:05:11 -0400

    files yeeeee

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route exact path='/services' element={<Services/>} />
+          <Route exact path='/products' element={<Products/>} />
+          <Route exact path='/sign-up' element={<SignUp/>} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
