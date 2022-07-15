fed8c3d49b55f8a09d9583f37bc2fbd2c3f6d5be
skim73 <55114034+skim73@users.noreply.github.com>
2022-07-15 01:06:49 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/TRVL/precache-manifest.6b39460858ae6c56c48d90212ef2c7ad.js"
+  "./precache-manifest.539898b06109288ffa74ee985996cd6b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/TRVL/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
